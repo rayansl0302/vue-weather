e1f9b6b9845bc8fd99703fac7c0ebef2096a2d57
nilssoncjonas <nilssoncjonas@gmail.com>
2023-11-17 23:13:21 +0100

    added axios and implemented mapBox request functionality when typeing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,51 @@
-<script setup></script>
-
 <template>
-	<main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQurey"
+        @input="getSearchResults"
+        placeholder="Search for a city!"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        v-if="mapBoxSearchResults"
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+      >
+        <li
+          v-for="searchResults in mapBoxSearchResults"
+          :key="searchResults.id"
+          class="py-2 cursor-pointer"
+        >
+          {{ searchResults.place_name }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+const mapBoxAPIKey =
+  'pk.eyJ1IjoiYWp2YXJyZWxpc2giLCJhIjoiY2xwMzVucnVjMHpzNDJobmxhZG1yMG1nNSJ9.PaX71_k3j27gRifYO9OfHw'
+
+const searchQurey = ref('')
+const queryTimeout = ref(null)
+const mapBoxSearchResults = ref(null)
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value)
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQurey.value !== '') {
+      const res = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQurey.value}.json?access_token=${mapBoxAPIKey}&types=place`
+      )
+      mapBoxSearchResults.value = res.data.features
+      console.log(mapBoxSearchResults.value)
+      return
+    }
+    mapBoxSearchResults.value = null
+  }, 300)
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'weather-primary': '#00998A',
+        'weather-primary': '#00668A',
         'weather-secondary': '#004E71',
       },
     },
