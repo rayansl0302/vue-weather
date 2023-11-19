22eafee406382ab094bfb81c832269b80f9f7d77
nilssoncjonas <nilssoncjonas@gmail.com>
2023-11-19 09:08:29 +0100

    added forecast weatherData for next 24h

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -11,7 +11,7 @@
 			</p>
 		</div>
 		<!-- Weather Overview -->
-		<div class="flex flex-col items-center text-white py-12">
+		<div class="flex flex-col items-center text-white py-4">
 			<h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
 			<p class="text-sm">
 				{{
@@ -42,13 +42,66 @@
 				L {{ Math.round(weatherData.main.temp_min) }}&deg;c / H
 				{{ Math.round(weatherData.main.temp_max) }}&deg;c
 			</p>
-			<p class="capitalize">
-				{{ weatherData.weather[0].description }}
+			<p>
+				<i class="fa-solid fa-wind"></i> {{ weatherData.wind.speed }} m/s
+				<i
+					:style="{ transform: `rotate(${weatherData.wind.deg}deg)` }"
+					class="fa-solid fa-angles-down"
+				></i>
+			</p>
+			<p>
+				<i class="fa-regular fa-sun"></i>
+				{{
+					new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('sv-SE', {
+						timeStyle: 'short',
+					})
+				}}
+			</p>
+			<p>
+				<i class="fa-regular fa-moon"></i>
+				{{
+					new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('sv-SE', {
+						timeStyle: 'short',
+					})
+				}}
 			</p>
 		</div>
 	</div>
 	<hr class="border-white border-opacity-10 border w-full" />
-	<div class="flex flex-col items-center text-white py-12"></div>
+	<!-- Hourly Weather -->
+	<div class="max-w-screen-md w-full py-12">
+		<div class="mx-8 text-white">
+			<h2 class="mb-4">Kommande 24h</h2>
+			<div class="flex gap-10 overflow-x-scroll">
+				<div
+					v-for="hourData in forecastData.list.slice(0, 9)"
+					:key="hourData.dt"
+					class="flex flex-col gap-4 items-center"
+				>
+					<p class="whitespace-nowrap text-md">
+						{{
+							new Date(hourData.dt_txt).toLocaleTimeString('sv-SE', {
+								weekday: 'long',
+								hour: 'numeric',
+								minute: 'numeric',
+							})
+						}}
+					</p>
+					<img
+						class="w-auto h-[50px] object-cover"
+						:src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
+						alt=""
+					/>
+					<p class="text-xl">{{ Math.round(hourData.main.temp) }}&deg;c</p>
+					<p class="text-sm text-center">
+						{{ Math.round(hourData.main.feels_like) }}&deg;c
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<hr class="border-white border-opacity-10 border w-full" />
 </template>
 
 <script setup>
@@ -57,11 +110,12 @@ import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const API_KEY = import.meta.env.VITE_OPEN_WEATHER_KEY
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/'
 
-const getWeatherData = async () => {
+const getWeatherData = async (param) => {
 	try {
 		const weatherData = await axios.get(
-			`https://api.openweathermap.org/data/2.5/weather?lat=${route.query.lat}&lon=${route.query.lng}&appid=${API_KEY}&units=metric`
+			`${BASE_URL}${param}?lat=${route.query.lat}&lon=${route.query.lng}&appid=${API_KEY}&units=metric`
 		)
 		return weatherData.data
 	} catch (err) {
@@ -69,6 +123,6 @@ const getWeatherData = async () => {
 	}
 }
 
-const weatherData = await getWeatherData()
-console.log(weatherData)
+const weatherData = await getWeatherData('weather')
+const forecastData = await getWeatherData('forecast')
 </script>
