ce8aa0d74d8f93b31269f4e9e7614c0b12a08a8e
nilssoncjonas <nilssoncjonas@gmail.com>
2023-11-19 07:37:45 +0100

    added weatherData for ccurrent weather at the searched location

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,5 +1,54 @@
 <template>
-	<div></div>
+	<div class="flex flex-col flex-1 items-center">
+		<!-- Banner -->
+		<div
+			v-if="route.query.preview"
+			class="text-white p-4 bg-weather-secondary w-full text-center"
+		>
+			<p>
+				You are currently previewing this city, click the "+" icon to add this
+				city to your list.
+			</p>
+		</div>
+		<!-- Weather Overview -->
+		<div class="flex flex-col items-center text-white py-12">
+			<h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+			<p class="text-sm">
+				{{
+					new Date().toLocaleDateString('sv-SE', {
+						weekday: 'long',
+						day: '2-digit',
+						month: 'short',
+					})
+				}}
+				{{
+					new Date(weatherData.dt * 1000).toLocaleTimeString('sv-SE', {
+						timeStyle: 'short',
+					})
+				}}
+			</p>
+			<p class="text-8xl">{{ Math.round(weatherData.main.temp) }}&deg;c</p>
+			<img
+				class="w-[150px] h-auto"
+				:src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`"
+				:alt="`${weatherData.weather[0].description}`"
+				:title="`${weatherData.weather[0].description}`"
+			/>
+			<p class="text-xl">
+				Känns som
+				{{ Math.round(weatherData.main.feels_like) }}&deg;c
+			</p>
+			<p>
+				L {{ Math.round(weatherData.main.temp_min) }}&deg;c / H
+				{{ Math.round(weatherData.main.temp_max) }}&deg;c
+			</p>
+			<p class="capitalize">
+				{{ weatherData.weather[0].description }}
+			</p>
+		</div>
+	</div>
+	<hr class="border-white border-opacity-10 border w-full" />
+	<div class="flex flex-col items-center text-white py-12"></div>
 </template>
 
 <script setup>
